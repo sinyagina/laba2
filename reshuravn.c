ddb89ac51798f915e2854e3fd98080bc952c1baf
sinyagina <Sinyagina060897@mail.ru>
2016-05-24 19:09:38 +0600

    proverka

--- reshuravn.c
+++ reshuravn.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <math.h>
+
 void resh(double a, double b, double c)
 {
 	double d;
 	d = b * b - 4 * a * c;
+	if (d < 0)
+	{
+		printf("net kornei");
+	}
 }
 
 int main()
