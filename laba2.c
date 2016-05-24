0fc54479d1ce57a1e2237a15d1c280adcc97476d
sinyagina <Sinyagina060897@mail.ru>
2016-05-24 19:07:21 +0600

    koefficienti

--- laba2.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main()
-{
-	printf("hello world\n");
-	printf("goodbye world\n");
-	return 0;
-}
---
+++ reshuravn.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+	int a, b, c;
+	printf("vvedite a b c: \n");
+	scanf("%d %d %d", &a, &b, &c);
+	return 0;
+}
